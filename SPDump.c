0711c679801509ab3429c6995134424e5076dc90
Tchan0 <61758157+Tchan0@users.noreply.github.com>
2023-10-28 23:26:35 +0200

    formatting fixes

--- SPDump.c
+++ SPDump.c
@@ -5,13 +5,12 @@
 
 #define RAM_DEST 0x8C400000
 
-
 void getROMData (uint16_t* pdst, int len){
  uint16_t* psrc = (uint16_t*)0xA1000000;
 
  while (len--){
     *pdst++ = *(uint16_t*)psrc;
-	psrc++;
+    psrc++;
  }
 }
 
@@ -31,24 +30,22 @@ void dumpOneChip (char* filename, uint32_t offset, uint32_t bytesInOneChip){
  uint8_t* pdst = (uint8_t*)RAM_DEST;
 
  printf("file %s:\n", filename);
+ fout = fopen (filename, "wb");
+ if (!fout){
+    printf("unable to open file %s...\n", filename);
+    return;
+ } else {
+    printf("writing output file...\n");
+ }
 
  printf("fetching ROM data...\n");
  for (int i=0; i < numBanks; i++){
     getROMData ((uint16_t*)pdst, 0x10000 /2);
-	curBank++;
-	setBank(curBank);
-	pdst += 0x10000;
+    curBank++;
+    setBank(curBank);
+    pdst += 0x10000;
  }
-
- //write the data to PC file
- fout = fopen (filename, "wb");
- if (!fout){
-	printf("unable to open file %s...\n", filename);
-	return;
- } else {
-	printf("writing output file...\n");
- }
-
+ 
  fwrite ((void*)RAM_DEST, bytesInOneChip, 1, fout);
 
  fclose (fout);
@@ -61,36 +58,36 @@ void dumpFlashRomData (int mbitsize, int numChips){
  uint32_t offset = 0;
 
  for (int i = 0; i < numChips; i++){
-	switch (i){
-		case 0:
-			filename = "/pc/ic62";
-			break;
-		case 1:
-			filename = "/pc/ic63";
-			break;
-		case 2:
-			filename = "/pc/ic64";
-			break;
-		case 3:
-			filename = "/pc/ic65";
-			break;
-		case 4:
-			filename = "/pc/ic66s";
-			break;
-		case 5:
-			filename = "/pc/ic67s";
-			break;
-		case 6:
-			filename = "/pc/ic68s";
-			break;
-		case 7:
-			filename = "/pc/ic69s";
-			break;
-		default:
-			return;
+    switch (i){
+        case 0:
+            filename = "/pc/ic62";
+            break;
+        case 1:
+            filename = "/pc/ic63";
+            break;
+        case 2:
+            filename = "/pc/ic64";
+            break;
+        case 3:
+            filename = "/pc/ic65";
+            break;
+        case 4:
+            filename = "/pc/ic66s";
+            break;
+        case 5:
+            filename = "/pc/ic67s";
+            break;
+        case 6:
+            filename = "/pc/ic68s";
+            break;
+        case 7:
+            filename = "/pc/ic69s";
+            break;
+        default:
+            return;
 	}
     
-	dumpOneChip (filename, offset, bytesInOneChip);
+    dumpOneChip (filename, offset, bytesInOneChip);
     offset += bytesInOneChip;
  }
 
